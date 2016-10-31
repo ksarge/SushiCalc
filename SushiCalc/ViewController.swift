744bb088ba4850ea82ed62e74ab3ca6198cdd0bd
Kylesargent1 <kylesargent@live.com>
2016-10-31 14:19:13 -0700

    each add button correctly references the corresponding counter. Calc is not working anymore

--- SushiCalc/ViewController.swift
+++ SushiCalc/ViewController.swift
@@ -10,48 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    @IBOutlet weak var plate0Count: UILabel!
-//    @IBOutlet weak var plate1Count: UILabel!
-
-    @IBOutlet var plateCounters: [UILabel]!
+    /*
+     TODO: Put price fields in an array
+     TODO: Fix calc to run through each counter and price
+     TODO: Refactor variables to increase readibility
+     
+     TODO: Create secondary page that allows the changing of plate colors
+     */
     
+    @IBOutlet var plateCounters: [UILabel]!
     
     @IBOutlet weak var plate0Price: UITextField!
     @IBOutlet weak var plate1Price: UITextField!
-    
-
     @IBOutlet weak var totalPrice: UILabel!
     @IBOutlet weak var taxedTotalPrice: UILabel!
     @IBOutlet weak var tippedTotalPrice: UILabel!
     
-    
-    /* TODO: Find out a way to make different buttons utilize the same callbacks and then update the corresponding plateCount.
-     */
+
     
     @IBAction func addButtonPressed(_ sender: UIButton) {
         
+        let tag   = sender.tag
+        let count = Int(plateCounters[tag].text!)!
         
-        let x = Int(plateCounters[0].text!)!
-        
-        plateCounters[0].text = String(x + 1)
+        plateCounters[tag].text = String(count + 1)
         
         calc()
     }
 
     @IBAction func subButtonPressed(_ sender: UIButton) {
         
-        let x = Int(plateCounters[0].text!)!
+        let tag   = sender.tag
+        let count = Int(plateCounters[0].text!)!
         
-        if x <= 0 {
-            plateCounters[0].text = String(0)
+        if count <= 0 {
+            plateCounters[tag].text = String(0)
         } else {
-            plateCounters[0].text = String(x - 1)
+            plateCounters[tag].text = String(count - 1)
         }
         calc()
     }
     
     func calc() {
         let x = Double(plateCounters[0].text!)!
+
+        
         let p: Double! = Double(plate0Price.text!)!
         totalPrice.text = String(format: "$%.2f", x * p)
         taxedTotalPrice.text = String(format: "$%.2f", x * p * 1.095)
