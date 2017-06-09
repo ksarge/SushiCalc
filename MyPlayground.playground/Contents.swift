a531b59f80e070c453d9651536476a455fccc42e
Kylesargent1 <kylesargent@live.com>
2017-06-09 00:11:02 -0700

    Refactoring code to MVC

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+/** 
+ * ConwaySort: 
+ * sorts an array by ignoring it and then
+ * printing out a new, sorted array with its 
+ * own "Alternative Values"
+ * 
+ * If the new array does not appear sorted,
+ * you have been maniputlated by MSM
+ */
+
+// The array to be sorted
+
+print("15, 16, 17, 18, 19, 20")
---
+++ SushiCalc/Calculator.swift
@@ -0,0 +1,34 @@
+//
+//  Calculator.swift
+//  SushiCalc
+//
+//  Created by Kyle Sargent on 6/8/17.
+//  Copyright © 2017 Kyle Sargent. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    var platePrices = [Int]()
+    var plateCounts = [Int]()
+    
+    func update() {
+        
+    }
+    
+    func reset() {
+        
+    }
+    
+    func calculate() {
+        
+    }
+    
+    func add() {
+        
+    }
+    
+    func sub() {
+        
+    }
+}
